494cb70cd27f209374fa7176c08bec692e0cd970
fatemanazari <fatemanazari78@gmail.com>
2023-07-26 10:39:37 +0330

    Fix errors

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const bookLists = document.getElementById('book-lists');
 const books = document.getElementById('books');
 const contactInformation = document.getElementById('contact-info');
 
+// Arrow function for showing content
+const showContent = (contentToShow) => {
+  // Hide all content sections first
+  bookLists.classList.remove('show');
+  books.classList.remove('show');
+  contactInformation.classList.remove('show');
+
+  // Show the content section that was passed as an argument
+  contentToShow.classList.add('show');
+};
+
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -15,12 +26,12 @@ class Book {
 class AwesomeBookApp {
   constructor() {
     this.booksCollection = [];
-    this.addBookButton = document.querySelector("#addBookBtn");
-    this.titleInput = document.getElementById("titleInput");
-    this.authorInput = document.getElementById("authorInput");
-    this.bookListDiv = document.getElementById("bookList");
+    this.addBookButton = document.querySelector('#addBookBtn');
+    this.titleInput = document.getElementById('titleInput');
+    this.authorInput = document.getElementById('authorInput');
+    this.bookListDiv = document.getElementById('bookList');
 
-    this.addBookButton.addEventListener("click", this.addBook.bind(this));
+    this.addBookButton.addEventListener('click', this.addBook.bind(this));
     this.displayBooks();
   }
 
@@ -28,12 +39,12 @@ class AwesomeBookApp {
     const title = this.titleInput.value.trim();
     const author = this.authorInput.value.trim();
 
-    if (title !== "" && author !== "") {
+    if (title !== '' && author !== '') {
       const newBook = new Book(title, author);
       this.booksCollection.push(newBook);
 
-      this.titleInput.value = "";
-      this.authorInput.value = "";
+      this.titleInput.value = '';
+      this.authorInput.value = '';
 
       this.displayBooks();
     }
@@ -45,19 +56,19 @@ class AwesomeBookApp {
   }
 
   displayBooks = () => {
-    this.bookListDiv.innerHTML = ""; // Clear the existing list
+    this.bookListDiv.innerHTML = ''; // Clear the existing list
 
-    for (const book of this.booksCollection) {
-      const listItem = document.createElement("li"); // Create a new <li> element
+    this.booksCollection.forEach((book) => {
+      const listItem = document.createElement('li'); // Create a new <li> element
 
-      const bookElement = document.createElement("p");
+      const bookElement = document.createElement('p');
       bookElement.classList.add('para');
       bookElement.textContent = `"${book.title}" by ${book.author}`;
 
-      const removeButton = document.createElement("button");
+      const removeButton = document.createElement('button');
       removeButton.classList.add('remove-btn');
-      removeButton.textContent = "Remove";
-      removeButton.addEventListener("click", () => {
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', () => {
         this.removeBook(this.booksCollection.indexOf(book));
       });
 
@@ -65,20 +76,20 @@ class AwesomeBookApp {
       listItem.appendChild(removeButton); // Append the <button> element to the <li> element
 
       this.bookListDiv.appendChild(listItem); // Finally, append the <li> element to the bookListDiv
-    }
+    });
 
     const now = new Date();
     const formattedDateTime = now.toLocaleString();
 
     // Update the content of the "currentDateTime" element
-    const currentDateTimeElement = document.getElementById("currentDateTime");
+    const currentDateTimeElement = document.getElementById('currentDateTime');
     currentDateTimeElement.classList.add('date-time');
     currentDateTimeElement.textContent = formattedDateTime;
   }
 }
 
-// Initialize the app
-const app = new AwesomeBookApp();
+// Initialize the app without assigning to a variable
+(() => new AwesomeBookApp())();
 
 lists.addEventListener('click', (event) => {
   event.preventDefault();
@@ -94,14 +105,3 @@ contact.addEventListener('click', (event) => {
   event.preventDefault();
   showContent(contactInformation);
 });
-
-// Arrow function for showing content
-const showContent = (contentToShow) => {
-  // Hide all content sections first
-  bookLists.classList.remove('show');
-  books.classList.remove('show');
-  contactInformation.classList.remove('show');
-
-  // Show the content section that was passed as an argument
-  contentToShow.classList.add('show');
-};
